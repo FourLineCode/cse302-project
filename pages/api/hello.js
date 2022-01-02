bc0e71da4b2bfb03d3f07a8a45bb372077c6aa26
FourLineCode <akmal3535.ah@gmail.com>
2022-01-02 20:11:49 +0600

    db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,8 @@
-import { db } from "../../lib/db";
+import { query } from "../../lib/db";
 
 export default async function handler(req, res) {
     try {
-        const data = await db.query(`SELECT * FROM User WHERE 1`);
-        db.end();
+        const data = await query(`SELECT * FROM User WHERE 1`);
         res.status(200).json(data);
     } catch (error) {
         console.log(error);
